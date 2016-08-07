9a9c0c1198ff4c49d965112b9ce40168d510d40b
bradpatton <bradpatton3.14@gmail.com>
2016-08-07 11:52:39 -0500

    on the road again

---
+++ teachersPet/ClassesCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ClassesCollectionViewCell.swift
+//  teachersPet
+//
+//  Created by Bradley Patton on 8/7/16.
+//  Copyright © 2016 Bradley Patton. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ClassesCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+}
---
+++ teachersPet/SplitViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SplitViewController.swift
+//  teachersPet
+//
+//  Created by Bradley Patton on 8/6/16.
+//  Copyright © 2016 Bradley Patton. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem()
+        navigationItem.leftItemsSupplementBackButton = true
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
